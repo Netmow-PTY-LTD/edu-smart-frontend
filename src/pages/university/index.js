f76f1854039256d179aa4ee71097dd07d4d15187
Ahsun-ullah <ahsunrahim@gmail.com>
2024-12-05 16:10:34 +0600

    push

--- src/pages/university/index.js
+++ src/pages/university/index.js
@@ -6,6 +6,17 @@ export default function UniversityHome() {
   return (
     <UniversityLayout>
       <HeroHome />
+      <section className="sqdk-about">
+        <div className="container">
+          <div className="sqdk-about-content">
+            <div className="sqdk-about-left-content">
+              <div className="sqdk-about-text-temp">
+                <h2>Our </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </UniversityLayout>
   );
 }
