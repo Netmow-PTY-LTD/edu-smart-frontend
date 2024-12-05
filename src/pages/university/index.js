66288ade1464e86872acd9ec396538506a5ecd06
Ahsun-ullah <ahsunrahim@gmail.com>
2024-12-05 17:52:49 +0600

    push

--- src/pages/university/index.js
+++ src/pages/university/index.js
@@ -1,6 +1,7 @@
 import AboutUniversity from '@/components/clientSite/university/AboutUniversity';
 import HeroHome from '@/components/clientSite/university/HeroHome';
 import UniversityLayout from '@/components/clientSite/university/UniversityLayout';
+import Image from 'next/image';
 import React from 'react';
 
 export default function UniversityHome() {
@@ -8,13 +9,59 @@ export default function UniversityHome() {
     <UniversityLayout>
       <HeroHome />
       <AboutUniversity />
-      <section className="sqdk-about">
+      <section className="sponsors-area  mt-5">
         <div className="container">
-          <div className="sqdk-about-content">
-            <div className="sqdk-about-left-content">
-              <div className="sqdk-about-text-temp">
-                <h2>Our </h2>
-              </div>
+          <div className="sec-heading">
+            <h3>Our Picture Gallery</h3>
+            <p>
+              Malaysia started focusing on the development of telecommunication,
+              Telekom Malaysia Berhad (TM) took a leap of faith by establishing
+              the first private-owned higher learning institute.
+            </p>
+
+            <div className="my-5 university_gallery">
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/1st-element.jpeg'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/second-element.png'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/third-element.jpeg'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/fourth-element.png'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/fivth-element.png'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/sixth-element.jpeg'}
+                alt=""
+              />
+              <Image
+                width={500}
+                height={500}
+                src={'/assets/university-gallery/seventh-element.png'}
+                alt=""
+              />
             </div>
           </div>
         </div>
