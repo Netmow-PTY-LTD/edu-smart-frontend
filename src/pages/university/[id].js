603d6673960ff5c7abaccb24c0b06ed9467a13ca
Ahsun-ullah <ahsunrahim@gmail.com>
2024-12-06 12:54:54 +0600

    many section done

--- src/pages/university/[id].js
+++ src/pages/university/[id].js
@@ -1,6 +1,8 @@
 import AboutUniversity from '@/components/clientSite/university/AboutUniversity';
 import HeroHome from '@/components/clientSite/university/HeroHome';
+import UniversityFAQ from '@/components/clientSite/university/UniversityFAQ';
 import UniversityLayout from '@/components/clientSite/university/UniversityLayout';
+import UniversityPictureGallery from '@/components/clientSite/university/UniversityPictureGallery';
 import { useGetAllUniversityQuery } from '@/slice/services/public/university/publicUniveristyService';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -22,63 +24,9 @@ export default function SingleUniversityHome() {
     <UniversityLayout>
       <HeroHome university={university} />
       <AboutUniversity university={university} />
-      <section className="sponsors-area  mt-5">
-        <div className="container">
-          <div className="sec-heading">
-            <h3>Our Picture Gallery</h3>
-            <p>
-              Malaysia started focusing on the development of telecommunication,
-              Telekom Malaysia Berhad (TM) took a leap of faith by establishing
-              the first private-owned higher learning institute.
-            </p>
-
-            <div className="my-5 university_gallery">
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/1st-element.jpeg'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/second-element.png'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/third-element.jpeg'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/fourth-element.png'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/fivth-element.png'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/sixth-element.jpeg'}
-                alt=""
-              />
-              <Image
-                width={500}
-                height={500}
-                src={'/assets/university-gallery/seventh-element.png'}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      
+      <UniversityPictureGallery />
+      <UniversityFAQ />
     </UniversityLayout>
   );
 }
--- src/pages/university/index.js
+++ src/pages/university/index.js
@@ -2,9 +2,8 @@ import AboutUniversity from '@/components/clientSite/university/AboutUniversity'
 import HeroHome from '@/components/clientSite/university/HeroHome';
 import UniversityFAQ from '@/components/clientSite/university/UniversityFAQ';
 import UniversityLayout from '@/components/clientSite/university/UniversityLayout';
-import { useGetAllUniversityQuery } from '@/slice/services/public/university/publicUniveristyService';
-import Image from 'next/image';
 import UniversityPictureGallery from '@/components/clientSite/university/UniversityPictureGallery';
+import { useGetAllUniversityQuery } from '@/slice/services/public/university/publicUniveristyService';
 import React from 'react';
 
 export default function UniversityHome() {
@@ -13,8 +12,6 @@ export default function UniversityHome() {
     <UniversityLayout>
       <HeroHome />
       <AboutUniversity />
-      <UniversityPictureGallery />
-      <UniversityFAQ />
     </UniversityLayout>
   );
 }
