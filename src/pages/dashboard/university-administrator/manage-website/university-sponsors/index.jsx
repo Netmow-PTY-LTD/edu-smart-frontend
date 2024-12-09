0cb6dc2b5823d8b43d51587eb869af5b82dc48c4
rabby.dev <rabby.netmow@gmail.com>
2024-12-09 16:32:05 +0600

    added manage-webiste all form filed page

---
+++ src/pages/dashboard/university-administrator/manage-website/university-sponsors/index.jsx
@@ -0,0 +1,51 @@
+import SubmitButton from '@/components/common/formField/SubmitButton';
+import TextArea from '@/components/common/formField/TextAreaField';
+import Layout from '@/components/layout';
+import { Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import * as Yup from 'yup';
+import { Card, Col, Row } from 'reactstrap';
+
+const UniversitySponsors = () => {
+  const [initialValues, setInitialValues] = useState({});
+  const validationSchema = Yup.object({});
+
+  const onSubmit = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <Layout>
+      <div className="page-content">
+        <div className="h-100">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Row>
+                 
+                 
+                  <Col md={12} xl={12}>
+                    <div className="my-4">
+                      <SubmitButton
+                        isSubmitting={isSubmitting}
+                        formSubmit={'formSubmit'}
+                      >
+                        {'Add Sponsor'}
+                      </SubmitButton>
+                    </div>
+                  </Col>
+                </Row>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default UniversitySponsors;
