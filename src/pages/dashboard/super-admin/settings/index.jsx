2d197ff6244c234e14a9ca777348cd987b8501cd
rabby.dev <rabby.netmow@gmail.com>
2024-12-23 15:46:00 +0600

    added settings route with layout

---
+++ src/pages/dashboard/super-admin/settings/index.jsx
@@ -0,0 +1,17 @@
+import Layout from '@/components/layout';
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <Layout>
+            <div className="page-content">
+                <div className="h-100">
+                    <h2>Settings</h2>
+                </div>
+            </div>
+            
+        </Layout>
+    );
+};
+
+export default Settings;
