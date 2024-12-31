41e4f60538e786b4d875f71a3f3377fb0c36e038
rabby.dev <rabby.netmow@gmail.com>
2024-12-31 12:23:52 +0600

    create paypal payment settings ui

--- src/components/sAdminDashboard/Settings/PaymentBox/PaymentSettings.jsx
+++ src/components/sAdminDashboard/Settings/PaymentBox/PaymentSettings.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import StripeSettings from './StripeSettings';
 import { Col, Row } from 'reactstrap';
+import StripeSettings from './StripeSettings';
 import SSLCommerceSettings from './SSLCommerceSettings';
+import PaypalSettings from './PaypalSettings';
 
 const PaymentSettings = () => {
   return (
     <Row>
-     <Col lg={6} >
-     <StripeSettings/>
-     </Col>
-     <Col lg={6} >
-     <SSLCommerceSettings/>
-     </Col>
-    
+      <Col lg={6}>
+        <StripeSettings />
+      </Col>
+      <Col lg={6}>
+        <PaypalSettings/>
+      </Col>
+      <Col lg={6}>
+        <SSLCommerceSettings />
+      </Col>
     </Row>
   );
 };
