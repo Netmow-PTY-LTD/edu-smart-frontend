6930e1ad55ac525237ec3676d4ec3cf71c154dd6
rabby.dev <rabby.netmow@gmail.com>
2024-12-31 12:19:30 +0600

    create ssl commerce payment settings ui

--- src/components/sAdminDashboard/Settings/PaymentBox/PaymentSettings.jsx
+++ src/components/sAdminDashboard/Settings/PaymentBox/PaymentSettings.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import StripeSettings from './StripeSettings';
 import { Col, Row } from 'reactstrap';
+import SSLCommerceSettings from './SSLCommerceSettings';
 
 const PaymentSettings = () => {
   return (
     <Row>
      <Col lg={6} >
      <StripeSettings/>
+     </Col>
+     <Col lg={6} >
+     <SSLCommerceSettings/>
      </Col>
     
     </Row>
