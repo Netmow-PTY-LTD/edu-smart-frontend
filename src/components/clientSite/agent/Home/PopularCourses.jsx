b39f2561576be1021104268532970c7399375243
rabby.dev <rabby.netmow@gmail.com>
2025-01-01 11:40:38 +0600

    delete code and modifed new section popular courses

---
+++ src/components/clientSite/agent/Home/PopularCourses.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import React from 'react';
+
+const PopularCourses = () => {
+  return (
+    <section className="popular-coureses-section ">
+      <div className="container">
+        <header className="popular-coureses-heading">
+          <h5 className="popular-coureses-subtitle">Looking For Most Popular Programs?</h5>
+          <h4 className="popular-coureses-title">
+          Discover The Most Popular Courses
+          That Shape Your Future
+          </h4>
+        </header>
+
+        <article className="popular-coureses-content">
+         
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default PopularCourses;
--- src/pages/agent/index.jsx
+++ src/pages/agent/index.jsx
@@ -1,7 +1,7 @@
 import AgentLayout from '@/components/clientSite/agent/AgentLayout';
-import HomeMission from '@/components/clientSite/agent/Home/HomeMission';
 import HomeSlider from '@/components/clientSite/agent/Home/HomeSlider';
 import HomeUniversities from '@/components/clientSite/agent/Home/HomeUniversities';
+import PopularCourses from '@/components/clientSite/agent/Home/PopularCourses';
 import React from 'react';
 
 export default function AgentHome() {
@@ -9,7 +9,7 @@ export default function AgentHome() {
     <AgentLayout>
       <HomeSlider />
       <HomeUniversities />
-      <HomeMission/>
+      <PopularCourses/>
     </AgentLayout>
   );
 }
