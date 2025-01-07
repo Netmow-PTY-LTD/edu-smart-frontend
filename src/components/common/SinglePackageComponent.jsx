20cc295dd3c931fc87526be4ebb9249220ac306e
Ahsun-ullah <ahsunrahim@gmail.com>
2025-01-07 16:42:45 +0600

    create ssl commerz form ui in super admin dashboard

--- src/components/common/SinglePackageComponent.jsx
+++ src/components/common/SinglePackageComponent.jsx
@@ -4,71 +4,79 @@ import React from 'react';
 
 const SinglePackageComponent = ({ togEditOpenModal }) => {
   return (
-    <div className="sqdk-single-pricing-table position-relative">
-      <div
-        style={{
-          position: 'absolute',
-          top: '0',
-          right: '0',
-        }}
-        className="mt-2 me-2 text-primary"
-      >
-        <i class="ri-fire-fill text-danger fw-bold fs-1"></i>
-        Hot Offer 50%
-      </div>
-      <div className="pricing-table-heading d-flex align-items-center justify-content-between mt-3">
-        <h4>{'Introducer Associate'}</h4>
-        <div className="icon">
-          <Image src={userDummyImage} alt="alt-image" width={50} height={50} />
-        </div>
-      </div>
-      <div className="price">
-        <p>
-          <span>MYR</span>
-          <small className="ms-3 text-primary">0</small>
-          <small>/Month</small>
-        </p>
-      </div>
-
-      <div className="pricing-lists">
-        <ul>
-          <li className="d-flex align-items-center gap-2">
-            <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
-            <span className="text-primary fw-semibold">Commission 50%</span>
-          </li>
-          <li className="d-flex align-items-center gap-2">
-            <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
-            <span className="text-primary fw-semibold">Minimum Files 5</span>
-          </li>
-          <li className="d-flex align-items-center gap-2">
-            <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
-            <span className="text-primary fw-semibold">
-              Auto Commission Deduct
-            </span>
-          </li>
-          <li className="d-flex align-items-center gap-2">
-            <i class="ri-close-circle-fill text-danger fs-1"></i>
-            <span className="text-primary fw-semibold">Yearly Bonus</span>
-          </li>
-          <li className="d-flex align-items-center gap-2">
-            <i class="ri-close-circle-fill text-danger fs-1"></i>
-            <span className="text-primary fw-semibold">Family Trip</span>
-          </li>
-        </ul>
-      </div>
-      {togEditOpenModal ? (
+    <>
+      <div className="sqdk-single-pricing-table position-relative">
         <div
-          onClick={togEditOpenModal}
-          className="d-flex align-items-center justify-content-center button hstack py-2"
+          style={{
+            position: 'absolute',
+            top: '0',
+          }}
+          className="mt-2 me-2 text-danger text-center fw-bold"
         >
-          <span className="text-center">Edit Package</span>
+          <i className="ri-fire-fill text-danger fw-bold fs-1"></i>
+          <span className="ms-2">Hot Offer 50% Extra Commission</span>
         </div>
-      ) : (
-        <div className="d-flex align-items-center justify-content-center button hstack py-2">
-          <span className="text-center fw-medium">Upgrade</span>
+        <div className="d-flex align-items-center justify-content-between mt-3 gap-5">
+          <h1 className="text-secondary-alt">{'Introducer Associate'}</h1>
+          <div className="icon">
+            <Image
+              src={userDummyImage}
+              alt="alt-image"
+              width={50}
+              height={50}
+              className="rounded-circle avatar-md img-thumbnail user-profile-image"
+            />
+          </div>
         </div>
-      )}
-    </div>
+
+        <div className="price">
+          <p>
+            <span>MYR</span>
+            <small className="ms-3 text-primary">0</small>
+            <small>/Month</small>
+          </p>
+        </div>
+
+        <div className="pricing-lists">
+          <ul>
+            <li className="d-flex align-items-center gap-2">
+              <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
+              <span className="text-primary fw-semibold">Commission 50%</span>
+            </li>
+            <li className="d-flex align-items-center gap-2">
+              <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
+              <span className="text-primary fw-semibold">Minimum Files 5</span>
+            </li>
+            <li className="d-flex align-items-center gap-2">
+              <i class="ri-checkbox-circle-fill fs-1 third-color"></i>
+              <span className="text-primary fw-semibold">
+                Auto Commission Deduct
+              </span>
+            </li>
+            <li className="d-flex align-items-center gap-2">
+              <i class="ri-close-circle-fill text-danger fs-1"></i>
+              <span className="text-primary fw-semibold">Yearly Bonus</span>
+            </li>
+            <li className="d-flex align-items-center gap-2">
+              <i class="ri-close-circle-fill text-danger fs-1"></i>
+              <span className="text-primary fw-semibold">Family Trip</span>
+            </li>
+          </ul>
+        </div>
+        {togEditOpenModal ? (
+          <div
+            onClick={togEditOpenModal}
+            className="d-flex align-items-center justify-content-center button hstack py-2"
+          >
+            <span className="text-center">Edit Package</span>
+          </div>
+        ) : (
+          <div className="d-flex align-items-center justify-content-center button hstack py-2">
+            <span className="text-center fw-medium">Upgrade</span>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/sAdminDashboard/Settings/PaymentBox/SSLCommerzSettings.jsx
+++ src/components/sAdminDashboard/Settings/PaymentBox/SSLCommerzSettings.jsx
@@ -1,16 +1,20 @@
 import PasswordField from '@/components/common/formField/PasswordField';
 import SingleSelectField from '@/components/common/formField/SingleSelectField';
 import SubmitButton from '@/components/common/formField/SubmitButton';
-import {
-  useGetAgentStripeSettingsQuery,
-  useUpdateAgentStripeSettingsMutation,
-} from '@/slice/services/agent/agentSettingsService';
-import { Form, Formik } from 'formik';
-import { useEffect, useState } from 'react';
+import { Formik } from 'formik';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
-import { Card, CardBody, CardHeader, CardTitle, Col, Row } from 'reactstrap';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Col,
+  Form,
+  Row,
+} from 'reactstrap';
 import * as Yup from 'yup';
-const StripeSettings = () => {
+const SSLCommerzSettings = () => {
   const [initialValues, setInitialValues] = useState({
     publishable_key_live: '',
     secret_key_live: '',
@@ -19,39 +23,39 @@ const StripeSettings = () => {
     mode: '',
   });
 
-  const { data: agentStripeData, refetch: agentStripeRefatch } =
-    useGetAgentStripeSettingsQuery();
+  // const { data: sslCommerzData, refetch: sslCommerzRefatch } =
+  //   useGetSSLCommerzSettingsQuery();
 
-  const [updateAgentStripeSettings] = useUpdateAgentStripeSettingsMutation();
+  // const [updateAgentStripeSettings] = useUpdateSSLCommerzSettingsMutation();
 
-  useEffect(() => {
-    if (agentStripeData?.data?._id) {
-      setInitialValues({
-        publishable_key_live: agentStripeData?.data?.publishable_key_live || '',
-        secret_key_live: agentStripeData?.data?.secret_key_live || '',
-        publishable_key_test: agentStripeData?.data?.publishable_key_test || '',
-        secret_key_test: agentStripeData?.data?.secret_key_test || '',
-        mode: agentStripeData?.data?.mode || '',
-      });
-    }
-  }, [
-    agentStripeData?.data?._id,
-    agentStripeData?.data?.mode,
-    agentStripeData?.data?.publishable_key_live,
-    agentStripeData?.data?.publishable_key_test,
-    agentStripeData?.data?.secret_key_live,
-    agentStripeData?.data?.secret_key_test,
-  ]);
+  // useEffect(() => {
+  //   if (sslCommerzData?.data?._id) {
+  //     setInitialValues({
+  //       publishable_key_live: sslCommerzData?.data?.publishable_key_live || '',
+  //       secret_key_live: sslCommerzData?.data?.secret_key_live || '',
+  //       publishable_key_test: sslCommerzData?.data?.publishable_key_test || '',
+  //       secret_key_test: sslCommerzData?.data?.secret_key_test || '',
+  //       mode: sslCommerzData?.data?.mode || '',
+  //     });
 
-  // console.log(initialValues);
+  //
+  //   }
+  // }, [
+  //   sslCommerzData?.data?._id,
+  //   sslCommerzData?.data?.mode,
+  //   sslCommerzData?.data?.publishable_key_live,
+  //   sslCommerzData?.data?.publishable_key_test,
+  //   sslCommerzData?.data?.secret_key_live,
+  //   sslCommerzData?.data?.secret_key_test,
+  // ]);
 
   const validationSchema = Yup.object({
     stripeKey: Yup.string()
-      .required('Stripe Key is required')
-      .min(10, 'Stripe Key should be at least 10 characters long'),
+      .required(' Key is required')
+      .min(10, ' Key should be at least 10 characters long'),
     stripeSecret: Yup.string()
-      .required('Stripe Secret is required')
-      .min(10, 'Stripe Secret should be at least 10 characters long'),
+      .required(' Secret is required')
+      .min(10, ' Secret should be at least 10 characters long'),
   });
 
   const handleSubmit = async (values, { setSubmitting }) => {
@@ -59,13 +63,13 @@ const StripeSettings = () => {
     console.log('Form Value:', data);
     try {
       setSubmitting(true);
-      const result = await updateAgentStripeSettings(data).unwrap();
-      if (result?.success) {
-        toast.success('Stripe Settings Updated Successfully');
-        agentStripeRefatch();
-      } else {
-        toast.error('Error during form submission');
-      }
+      //  const result = await useUpdateSSLCommerzSettingsMutation(data).unwrap();
+      //  if (result?.success) {
+      //    toast.success('Stripe Settings Updated Successfully');
+      //    agentStripeRefatch();
+      //  } else {
+      //    toast.error('Error during form submission');
+      //  }
     } catch (error) {
       console.error('Error during form submission:', error);
       toast.error('Error during form submission');
@@ -83,7 +87,7 @@ const StripeSettings = () => {
     <>
       <Card className="mt-5">
         <CardHeader>
-          <CardTitle tag="h5"> Stripe Settings </CardTitle>
+          <CardTitle tag="h5"> SSL Commerz Settings </CardTitle>
         </CardHeader>
         <CardBody className="p-5">
           <Formik
@@ -97,29 +101,29 @@ const StripeSettings = () => {
                   <Col lg={6}>
                     <PasswordField
                       name={'publishable_key_live'}
-                      label={'Stripe Live Key'}
-                      placeholder={'Enter Stripe Key '}
+                      label={'SSL Live Key'}
+                      placeholder={'Enter SSL Key '}
                     />
                   </Col>
                   <Col lg={6}>
                     <PasswordField
                       name={'secret_key_live'}
-                      label={'Stripe Live Secret'}
-                      placeholder={'Enter Stripe Secret '}
+                      label={'SSL Live Secret'}
+                      placeholder={'Enter SSL Secret '}
                     />
                   </Col>
                   <Col lg={6}>
                     <PasswordField
                       name={'publishable_key_test'}
-                      label={'Stripe Test Key'}
-                      placeholder={'Enter Stripe Key '}
+                      label={'SSL Test Key'}
+                      placeholder={'Enter SSL Key '}
                     />
                   </Col>
                   <Col lg={6}>
                     <PasswordField
                       name={'secret_key_test'}
-                      label={'Stripe Test Secret'}
-                      placeholder={'Enter Stripe Secret '}
+                      label={'SSL Test Secret'}
+                      placeholder={'Enter SSL Secret '}
                     />
                   </Col>
                   <Col lg={6}>
@@ -147,4 +151,4 @@ const StripeSettings = () => {
   );
 };
 
-export default StripeSettings;
+export default SSLCommerzSettings;
