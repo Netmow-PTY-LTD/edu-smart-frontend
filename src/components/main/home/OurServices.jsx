a2d7ee560f9b9e85591655c344cb616baab5ca47
rabby.dev <rabby.netmow@gmail.com>
2025-02-05 12:32:40 +0600

    make  ourservices section and add this section heading and bg image

---
+++ src/components/main/home/OurServices.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const OurServices = () => {
+  return (
+    <div
+      className="bg-black"
+      style={{
+        backgroundImage: 'url("/assets/images/landing/ourServices/Vector.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="section-heading">
+        <h3>What We Do?</h3>
+        <h2 className="text-light">
+          EduSmart Guiding For Global Education Journey
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default OurServices;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
-
 import HeroSection from '@/components/main/home/HeroSection';
+import OurServices from '@/components/main/home/OurServices';
 import Layout from '@/components/main/layout';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,6 +13,7 @@ const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
+      <OurServices />
     </Layout>
   );
 };
