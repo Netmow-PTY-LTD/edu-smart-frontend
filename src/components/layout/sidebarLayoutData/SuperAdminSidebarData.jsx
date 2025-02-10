4934471fff51fde3beb971c7982b159ddf310a2b
rabby.dev <rabby.netmow@gmail.com>
2025-02-10 14:36:29 +0600

    added recent appliation table in super admin

--- src/components/layout/sidebarLayoutData/SuperAdminSidebarData.jsx
+++ src/components/layout/sidebarLayoutData/SuperAdminSidebarData.jsx
@@ -77,6 +77,13 @@ const SuperAdminSidebarData = () => {
           link: '/dashboard/super-admin/university-management/add-university',
           parentId: 'universities',
         },
+        {
+          id: 'recent-application',
+          label: 'Recent Application',
+          icon: 'ri-school-fill',
+          link: '/dashboard/super-admin/university-management/recent-application',
+          parentId: 'universities',
+        },
       ],
     },
     {
