501ffc1f3b21e2eb360e73bdd7b22200874d6981
rabby.dev <rabby.netmow@gmail.com>
2025-02-21 15:39:32 +0600

    change modal form business logic

--- src/components/agentDashboard/studentManagement/singleStudentProfile/modal/DocumentRequestModalForm.jsx
+++ src/components/agentDashboard/studentManagement/singleStudentProfile/modal/DocumentRequestModalForm.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Modal, ModalHeader, ModalBody, Row, Col } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Form } from 'formik';
 import SubmitButton from '@/components/common/formField/SubmitButton';
-import SingleSelectField from '@/components/common/formField/SingleSelectField';
 import TextArea from '@/components/common/formField/TextAreaField';
-import SingleSelectFieldForAgent from './SingleSelectFieldForAgent';
 import { useGetAllDocumentListQuery } from '@/slice/services/common/commonDocumentService';
+import CreatableSelect from 'react-select/creatable';
 
 const DocumentRequestModalForm = ({
   formHeader,
@@ -15,92 +14,106 @@ const DocumentRequestModalForm = ({
   validationSchema,
   onSubmit,
   formSubmit,
-  setInitialValues,
 }) => {
-  const { data: documentData, isLoading: documentLoading } =
-    useGetAllDocumentListQuery();
+  const { data: documentData } = useGetAllDocumentListQuery();
 
-  console.log();
-  const options = documentData.data.map((item) => ({
-    value: item._id,
-    label: item.title,
-  }));
-  // const options = [
-  //   { value: 'photograph', label: 'Photograph' },
-  //   { value: 'passport', label: 'Passport' },
-  //   { value: 'offer_letter', label: 'Offer Letter' },
-  //   { value: 'medical_certificate', label: 'Medical Certificate' },
-  //   { value: 'academic_certificate', label: 'Academic Certificate' },
-  //   { value: 'personal_bond', label: 'Personal Bond' },
-  //   { value: 'noc', label: 'NOC' },
-  //   { value: 'letter_of_eligibility', label: 'Letter of Eligibility' },
-  //   {
-  //     value: 'english_language_certificate',
-  //     label: 'English Language Certificate',
-  //   },
-  //   { value: 'others', label: 'Others' },
-  // ];
+  // Generate document options from API response
+  const options =
+    documentData?.data?.map((item) => ({
+      value: item.title, // Store title instead of ID for easier comparison
+      label: item.title,
+    })) || [];
+
   return (
-    <div>
-      <Modal isOpen={isOpen} centered size="md">
-        <ModalHeader toggle={onClose} className="fw-semibold text-black">
-          {formHeader}
-        </ModalHeader>
-        <ModalBody>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-          >
-            {({ isSubmitting }) => (
+    <Modal isOpen={isOpen} centered size="md">
+      <ModalHeader toggle={onClose} className="fw-semibold text-black">
+        {formHeader}
+      </ModalHeader>
+      <ModalBody>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {({ isSubmitting, values, setFieldValue }) => {
+            // Check if a new title is created
+            const isNewTitle =
+              values.title &&
+              !options.some((option) => option.value === values.title);
+
+            return (
               <Form>
                 <Row>
                   <Col lg={12}>
-                    <div className="ps-0">
-                      <Row>
-                        <Col md={12} xl={12}>
-                          <div className="mb-4">
-                            <SingleSelectFieldForAgent
-                              options={options}
-                              name="title"
-                              label="Document Title *"
-                              setInitialValues={setInitialValues}
-                            />
-                          </div>
-                        </Col>
-                        <Col md={12} xl={12}>
-                          <div className="">
-                            <TextArea
-                              name="description"
-                              label="Description *"
-                            />
-                          </div>
-                        </Col>
-                        <Col md={12} xl={12}>
-                          <div className="">
-                            <TextArea name="notes" label="Notes *" />
-                          </div>
-                        </Col>
-                        <Col md={12} xl={12}>
-                          <div className="my-4">
-                            <SubmitButton
-                              isSubmitting={isSubmitting}
-                              formSubmit="formSubmit"
-                            >
-                              {formSubmit}
-                            </SubmitButton>
-                          </div>
-                        </Col>
-                      </Row>
-                    </div>
+                    <Row>
+                      {/* Title Select Field */}
+                      <Col md={12} xl={12}>
+                        <div className="mb-3">
+                          <label className="form-label  mb-2">
+                            Document Title *
+                          </label>
+                          <CreatableSelect
+                            isClearable
+                            options={options}
+                            className="basic-multi-select"
+                            classNamePrefix="select"
+                            value={
+                              values.title
+                                ? { label: values.title, value: values.title }
+                                : null
+                            }
+                            onChange={(selectedOption) => {
+                              setFieldValue(
+                                'title',
+                                selectedOption ? selectedOption.value : ''
+                              );
+                            }}
+                            onCreateOption={(inputValue) => {
+                              setFieldValue('title', inputValue);
+                            }}
+                          />
+                        </div>
+                      </Col>
+
+                      {/* Show "Description" & "Notes" when a new title is created or "Others" is selected */}
+                      {isNewTitle && (
+                        <>
+                          <Col md={12} xl={12}>
+                            <div className="mb-3">
+                              <TextArea
+                                name="description"
+                                label="Document Description *"
+                              />
+                            </div>
+                          </Col>
+                          {/* <Col md={12} xl={12}>
+                            <div className="mb-3">
+                              <TextArea name="notes" label="Notes *" />
+                            </div>
+                          </Col> */}
+                        </>
+                      )}
+
+                      {/* Submit Button */}
+                      <Col md={12} xl={12}>
+                        <div className="my-4">
+                          <SubmitButton
+                            isSubmitting={isSubmitting}
+                            formSubmit="formSubmit"
+                          >
+                            {formSubmit}
+                          </SubmitButton>
+                        </div>
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </Form>
-            )}
-          </Formik>
-        </ModalBody>
-      </Modal>
-    </div>
+            );
+          }}
+        </Formik>
+      </ModalBody>
+    </Modal>
   );
 };
 
