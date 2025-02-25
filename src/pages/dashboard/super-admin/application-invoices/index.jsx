d4c5fff8d99c88a7ce14a428bc1bb0dbbca43bdd
shadiknetmow <shadik.netmow@gmail.com>
2025-02-25 15:18:53 +0600

    Course Update

--- src/pages/dashboard/super-admin/application-invoices/index.jsx
+++ src/pages/dashboard/super-admin/application-invoices/index.jsx
@@ -1,5 +1,6 @@
 import CommonTableComponent from '@/components/common/CommonTableComponent';
 import InvoicesComponentForMultipleData from '@/components/common/InvoicesComponentForMultipleData';
+import InvoicesComponentForMultipleDataTuitionFee from '@/components/common/InvoicesComponentForMultipleDataTuitionFee';
 import SearchComponent from '@/components/common/SearchComponent';
 import LoaderSpiner from '@/components/constants/Loader/LoaderSpiner';
 import Layout from '@/components/layout';
@@ -26,6 +27,7 @@ const ApplicationInvoiceInSuperAdmin = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [currentPage, setCurrentPage] = useState(0);
   const [openInvoiceModal, setOpenInvoiceModal] = useState(false);
+  const [openInvoiceModalTuition, setOpenInvoiceModalTuition] = useState(false);
   const [applicationId, setApplicationId] = useState('');
 
   const perPageData = 10;
@@ -159,7 +161,20 @@ const ApplicationInvoiceInSuperAdmin = () => {
               className="text-primary"
             >
               <i className="ri-eye-fill me-2"></i>
-              View Invoice
+              View Emgs Invoice
+            </div>
+            <div
+              onClick={() => {
+                if (item?._id) {
+                  setApplicationId(item?._id);
+                  setOpenInvoiceModalTuition(true);
+                  getSingleApplicationPaymentReportDataRefetch(item?._id);
+                }
+              }}
+              className="text-primary"
+            >
+              <i className="ri-eye-fill me-2"></i>
+              View Tuition Invoice
             </div>
           </DropdownItem>
         </DropdownMenu>
@@ -183,7 +198,7 @@ const ApplicationInvoiceInSuperAdmin = () => {
               <Card>
                 <CardHeader className="d-flex justify-content-between align-items-center">
                   <div className="text-primary fw-semibold fs-2">
-                    Application Payment Report
+                    Application Invoice
                   </div>
                   <SearchComponent
                     searchTerm={searchTerm}
@@ -212,6 +227,40 @@ const ApplicationInvoiceInSuperAdmin = () => {
               open={openInvoiceModal}
               close={() => {
                 setApplicationId(''), setOpenInvoiceModal(false);
+              }}
+              loading={getSingleApplicationPaymentReportDataLoading}
+              addressData={superAdminData}
+              billingAddressData={
+                getSingleApplicationPaymentReportData?.data?.applied_by
+              }
+              tableData={[getSingleApplicationPaymentReportData?.data]}
+              //   generatePDF,
+              printInvoice={printInvoice}
+              //   payButton,
+              //   goToPay,
+              //   chargesType,
+              // invoice=
+              //   superAdmin,
+              subtotal={
+                getSingleApplicationPaymentReportData?.data?.paid_amount
+              }
+              //   gst,
+              total={getSingleApplicationPaymentReportData?.data?.paid_amount}
+              currency={'MYR'}
+              payment_status={
+                getSingleApplicationPaymentReportData?.data?.application
+                  ?.payment_status
+              }
+              logoData={brandlogo}
+              invoice_no={getSingleApplicationPaymentReportData?.data}
+            />
+          }
+
+          {
+            <InvoicesComponentForMultipleDataTuitionFee
+              open={openInvoiceModalTuition}
+              close={() => {
+                setApplicationId(''), setOpenInvoiceModalTuition(false);
               }}
               loading={getSingleApplicationPaymentReportDataLoading}
               addressData={superAdminData}
