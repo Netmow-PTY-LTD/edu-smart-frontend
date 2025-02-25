9b6879166a469678253138a58d3cf334cfc2f4e1
Ahsun-ullah <ahsunrahim@gmail.com>
2025-02-25 11:09:53 +0600

    staff members update and delete api integration done

--- src/components/common/DeleteModal.jsx
+++ src/components/common/DeleteModal.jsx
@@ -9,9 +9,7 @@ const DeleteModal = ({
   id,
   handleDelete,
   gid,
-  deleteBtn,
-  handleSpecialDutyRosterDelete,
-  inactive,
+  userStatus,
   isloading,
 }) => {
   return (
@@ -19,17 +17,27 @@ const DeleteModal = ({
       <ModalBody className="py-5">
         <div className="mt-2 text-center">
           <Script src="https://cdn.lordicon.com/bhenfmcm.js"></Script>
-          <lord-icon
-            src="https://cdn.lordicon.com/gsqxdxog.json"
-            trigger="hover"
-            colors="primary:#f7b84b,secondary:#f06548"
-            style={{ width: '100px', height: '100px' }}
-          ></lord-icon>
+          {userStatus === 'active' ? (
+            <lord-icon
+              src="https://cdn.lordicon.com/hqvguthc.json"
+              trigger="hover"
+              colors="primary:#bed45e,secondary:#b5d336"
+              style={{ width: '100px', height: '100px' }}
+            ></lord-icon>
+          ) : (
+            <lord-icon
+              src="https://cdn.lordicon.com/gsqxdxog.json"
+              trigger="hover"
+              colors="primary:#f7b84b,secondary:#f06548"
+              style={{ width: '100px', height: '100px' }}
+            ></lord-icon>
+          )}
+
           <div className="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
             <h4 className="fs-2">Are you sure ?</h4>
             <p className="text-muted mx-4 mb-0">
-              {inactive
-                ? 'Are you sure you want to make this record inactive?'
+              {userStatus
+                ? `Are you sure you want to make this record ${userStatus}?`
                 : 'Are you sure you want to remove this record?'}
             </p>
           </div>
@@ -43,32 +51,19 @@ const DeleteModal = ({
           >
             Close
           </button>
-
-          {deleteBtn === 'specialEvents' ? (
-            isloading ? (
+          <div className="hstack button ">
+            {isloading ? (
               <Loader />
             ) : (
-              <button
-                type="button"
-                className="btn w-sm btn-danger fs-2"
+              <div
+                className={`w-sm  fs-2 ${userStatus === 'active' ? 'button px-3 py-2' : 'btn-secondary-alt px-3 py-2'}`}
                 id="delete-record"
-                onClick={() => handleSpecialDutyRosterDelete(id, gid)}
+                onClick={() => handleDelete(id, gid)}
               >
-                Yes, Delete It !
-              </button>
-            )
-          ) : isloading ? (
-            <Loader />
-          ) : (
-            <button
-              type="button"
-              className="btn w-sm btn-danger fs-2"
-              id="delete-record"
-              onClick={() => handleDelete(id, gid)}
-            >
-              {inactive ? 'Yes, Make It Inactive' : 'Yes, Delete It !'}
-            </button>
-          )}
+                {userStatus ? `Yes, Make It ${userStatus}` : 'Yes, Delete It !'}
+              </div>
+            )}
+          </div>
         </div>
       </ModalBody>
     </Modal>
--- src/components/sAdminDashboard/modals/SelectUserModalForSuperAdmin.jsx
+++ src/components/sAdminDashboard/modals/SelectUserModalForSuperAdmin.jsx
@@ -6,6 +6,7 @@ import PasswordField from '@/components/common/formField/PasswordField';
 import SingleSelectField from '@/components/common/formField/SingleSelectField';
 import SubmitButton from '@/components/common/formField/SubmitButton';
 import TextField from '@/components/common/formField/TextField';
+import LoaderSpiner from '@/components/constants/Loader/LoaderSpiner';
 import { brandlogo } from '@/utils/common/data';
 import { Form, Formik } from 'formik';
 import Image from 'next/image';
@@ -24,6 +25,7 @@ const SelectUserModalForSuperAdmin = ({
   closeModal,
   modalTitle,
   imagePreview,
+  isLoading,
 }) => {
   const options = useMemo(() => countryList().getData(), []);
 
@@ -54,150 +56,157 @@ const SelectUserModalForSuperAdmin = ({
       <ModalHeader toggle={() => closeModal()}>{modalTitle}</ModalHeader>
       <ModalBody>
         <div className="">
-          <Card className="p-4 p-md-5 add-university-card">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={handleSubmit}
-              enableReinitialize={true}
-            >
-              {({ isSubmitting, setFieldValue, resetForm }) => (
-                <Form>
-                  <Row>
-                    <Col lg={3}>
-                      <h4>Profile Photo</h4>
-                      <div
-                        id="profileimage"
-                        className="mb-5 profile-img text-center"
-                      >
-                        <div className="mb-3 profile-user">
-                          <Image
-                            src={imagePreview || brandlogo}
-                            alt="Profile Photo"
-                            width={200}
-                            height={200}
-                            className="rounded-circle avatar-xl img-thumbnail user-profile-image"
-                          />
-                        </div>
-                        <ImageField
-                          name="image"
-                          label="Upload Photo"
-                          handleImageChange={handleImageChange}
-                        />
-                        <button
-                          className="btn btn-danger w-100 mt-4 fw-semibold fs-14"
-                          onClick={() => {
-                            setImagePreview(null);
-                            setFieldValue('image', null);
-                          }}
+          {isLoading ? (
+            <LoaderSpiner />
+          ) : (
+            <Card className="p-4 p-md-5 add-university-card">
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+                enableReinitialize={true}
+              >
+                {({ isSubmitting, setFieldValue, resetForm }) => (
+                  <Form>
+                    <Row>
+                      <Col lg={3}>
+                        <h4>Profile Photo</h4>
+                        <div
+                          id="profileimage"
+                          className="mb-5 profile-img text-center"
                         >
-                          Remove
-                        </button>
-                      </div>
-                    </Col>
+                          <div className="mb-3 profile-user">
+                            <Image
+                              src={imagePreview || brandlogo}
+                              alt="Profile Photo"
+                              width={200}
+                              height={200}
+                              className="rounded-circle avatar-xl img-thumbnail user-profile-image"
+                            />
+                          </div>
+                          <ImageField
+                            name="image"
+                            label="Upload Photo"
+                            handleImageChange={handleImageChange}
+                          />
+                          <button
+                            className="btn btn-danger w-100 mt-4 fw-semibold fs-14"
+                            onClick={() => {
+                              setImagePreview(null);
+                              setFieldValue('image', null);
+                            }}
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      </Col>
 
-                    <Col lg={9}>
-                      <div className="ps-0 ps-lg-5">
-                        <Row>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <TextField
-                                name={'first_name'}
-                                label={'First Name'}
-                              />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <TextField
-                                name={'last_name'}
-                                label={'Last Name'}
-                              />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <EmailField name="email" label="Email" />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <NumberField
-                                name="phone"
-                                label="Contact Number"
-                              />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <PasswordField name="password" label="Password" />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <PasswordField
-                                name="confirm_password"
-                                label="Confirm Password"
-                              />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <TextField name="address" label="Address" />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="mb-3 ">
-                              <SingleSelectField
-                                name="select_role"
-                                label="Select Role"
-                                options={roleOptions}
-                              />
-                            </div>
-                          </Col>
+                      <Col lg={9}>
+                        <div className="ps-0 ps-lg-5">
+                          <Row>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <TextField
+                                  name={'first_name'}
+                                  label={'First Name'}
+                                />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <TextField
+                                  name={'last_name'}
+                                  label={'Last Name'}
+                                />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <EmailField name="email" label="Email" />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <NumberField
+                                  name="phone"
+                                  label="Contact Number"
+                                />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <PasswordField
+                                  name="password"
+                                  label="Password"
+                                />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <PasswordField
+                                  name="confirm_password"
+                                  label="Confirm Password"
+                                />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <TextField name="address" label="Address" />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="mb-3 ">
+                                <SingleSelectField
+                                  name="select_role"
+                                  label="Select Role"
+                                  options={roleOptions}
+                                />
+                              </div>
+                            </Col>
 
-                          <Col md={4} xl={6}>
-                            <div className="">
-                              <TextField name="city" label="City" />
-                            </div>
-                          </Col>
-                          <Col md={4} xl={6}>
-                            <div className="">
-                              <TextField name="state" label="State" />
-                            </div>
-                          </Col>
-                          <Col md={4} xl={6}>
-                            <div className="">
-                              <NumberField name="zip" label="Zip Code" />
-                            </div>
-                          </Col>
-                          <Col md={6} xl={6}>
-                            <div className="">
-                              <CountrySelectField
-                                name="country"
-                                label="Country"
-                                options={options}
-                              />
-                            </div>
-                          </Col>
-                          <Col md={12} xl={12}>
-                            <div className="my-4">
-                              <SubmitButton
-                                isSubmitting={isSubmitting}
-                                formSubmit={submitBtn}
-                              >
-                                {submitBtn}
-                              </SubmitButton>
-                            </div>
-                          </Col>
-                        </Row>
-                      </div>
-                    </Col>
-                  </Row>
-                </Form>
-              )}
-            </Formik>
-          </Card>
+                            <Col md={4} xl={6}>
+                              <div className="">
+                                <TextField name="city" label="City" />
+                              </div>
+                            </Col>
+                            <Col md={4} xl={6}>
+                              <div className="">
+                                <TextField name="state" label="State" />
+                              </div>
+                            </Col>
+                            <Col md={4} xl={6}>
+                              <div className="">
+                                <NumberField name="zip" label="Zip Code" />
+                              </div>
+                            </Col>
+                            <Col md={6} xl={6}>
+                              <div className="">
+                                <CountrySelectField
+                                  name="country"
+                                  label="Country"
+                                  options={options}
+                                />
+                              </div>
+                            </Col>
+                            <Col md={12} xl={12}>
+                              <div className="my-4">
+                                <SubmitButton
+                                  isSubmitting={isSubmitting}
+                                  formSubmit={submitBtn}
+                                >
+                                  {submitBtn}
+                                </SubmitButton>
+                              </div>
+                            </Col>
+                          </Row>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Form>
+                )}
+              </Formik>
+            </Card>
+          )}
         </div>
       </ModalBody>
     </Modal>
