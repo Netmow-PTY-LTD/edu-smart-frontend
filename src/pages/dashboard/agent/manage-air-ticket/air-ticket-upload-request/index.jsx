9df368bd72c175e912adac8509117d2fc6f278b2
rabby.dev <rabby.netmow@gmail.com>
2025-02-26 17:18:37 +0600

    added super admin request table

--- src/pages/dashboard/agent/manage-air-ticket/air-ticket-upload-request/index.jsx
+++ src/pages/dashboard/agent/manage-air-ticket/air-ticket-upload-request/index.jsx
@@ -10,17 +10,26 @@ import {
   useGetAllStudentsAirticketDocumentRequestQuery,
   useUpdateSingleAirTicketDocumentForAgentMutation,
 } from '@/slice/services/common/commonDocumentService';
-import { useUpdateSingleAirTicketDocumentForStudentMutation } from '@/slice/services/student/studentSubmitDocumentService';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Card, CardBody, CardHeader } from 'reactstrap';
 import * as Yup from 'yup';
 const StudentAirtTicketDocumentUploadRquestForAgent = () => {
-  const [searchTermForRequest, setSearchTermForRequest] = useState('');
+  const [searchTermForRequestForAgent, setSearchTermForRequestForAgent] =
+    useState('');
+  const [
+    searchTermForRequestForSuperAdmin,
+    setSearchTermForRequestForSuperAdmin,
+  ] = useState('');
   const [searchTermForSubmitedData, setSearchTermForSubmitedData] =
     useState('');
-  const [currentPageForRequest, setCurrentPageForRequest] = useState(0);
+  const [currentPageForRequestForAgent, setCurrentPageForRequestForAgent] =
+    useState(0);
+  const [
+    currentPageForRequestForSuparAdmin,
+    setCurrentPageForRequestForSuparAdmin,
+  ] = useState(0);
   const [currentPageForSubmittedData, setCurrentPageForSubmittedData] =
     useState(0);
   const [openModal, setOpenModal] = useState(false);
@@ -70,6 +79,16 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
     refetch: allDocumentRequestForAgentRefetch,
   } = useGetAllStudentsAirticketDocumentRequestQuery();
 
+  const superAdminAirtTicketDocRequestData =
+    allDocumentRequestForAgentData?.data?.filter(
+      (item) => item.requested_by.role === 'super_admin'
+    );
+
+  const agentAirtTicketDocRequestData =
+    allDocumentRequestForAgentData?.data?.filter(
+      (item) => item.requested_by.role === 'agent'
+    );
+
   const {
     data: allAirTicketDocumentSubmittedDataForAgentData,
     error: allAirTicketDocumentSubmittedForAgentError,
@@ -80,16 +99,28 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
   const [submitSingleDocumentForAgent] =
     useUpdateSingleAirTicketDocumentForAgentMutation();
   //  search input change function
-  const handleSearchChangeForRequest = (e) =>
-    setSearchTermForRequest(e.target.value);
+  const handleSearchChangeForRequestForSuperAdmin = (e) =>
+    setSearchTermForRequestForSuperAdmin(e.target.value);
+  const handleSearchChangeForRequestForAgent = (e) =>
+    setSearchTermForRequestForAgent(e.target.value);
   const handleSearchChangeForSubmittedData = (e) =>
     setSearchTermForSubmitedData(e.target.value);
 
+  // Filter data for search option SuperAdmin
+  const isfilteredDataForSuperAdmin =
+    superAdminAirtTicketDocRequestData?.length > 0 &&
+    superAdminAirtTicketDocRequestData.filter((item) =>
+      item?.title
+        ?.toLowerCase()
+        .includes(searchTermForRequestForSuperAdmin.toLowerCase())
+    );
   // Filter data for search option
-  const isfilteredData =
-    allDocumentRequestForAgentData?.data?.length > 0 &&
-    allDocumentRequestForAgentData?.data.filter((item) =>
-      item?.title?.toLowerCase().includes(searchTermForRequest.toLowerCase())
+  const isfilteredDataForAgent =
+    agentAirtTicketDocRequestData?.length > 0 &&
+    agentAirtTicketDocRequestData.filter((item) =>
+      item?.title
+        ?.toLowerCase()
+        .includes(searchTermForRequestForAgent.toLowerCase())
     );
 
   // Filter data for search option
@@ -161,7 +192,7 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
       const result = await submitSingleDocumentForAgent(finalData).unwrap();
       if (result) {
         toast.success(result?.message);
-        allDocumentRequestForAgentData();
+        allDocumentRequestForAgentRefetch();
         setOpenModal(!openModal);
       }
     } catch (error) {
@@ -447,18 +478,166 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
     },
   ];
 
+  const docRequestTableHeaderDataWithAction = [
+    {
+      title: 'SN',
+      key: 'sn',
+      render: (item, index) => (
+        <div>
+          <h5 className="fs-14 fw-medium text-capitalize">{index + 1}</h5>
+        </div>
+      ),
+    },
+    {
+      title: 'Student Name',
+      key: 'user',
+      render: (item) => (
+        <span className="d-flex flex-column text-capitalize">
+          {item?.user?.first_name && item?.user?.last_name ? (
+            <Link
+              href={`/dashboard/agent/student-management/single-student-for-agent/${item?.user?._id}?tab=6`}
+              className="text-primary text-decoration-none"
+            >
+              {`${item?.user?.first_name} ${item?.user?.last_name}`}
+            </Link>
+          ) : (
+            '-'
+          )}
+        </span>
+      ),
+    },
+
+    {
+      title: 'Doc Title',
+      key: 'title',
+      render: (item) => {
+        const newTitle = item?.title?.replace(/_/g, ' ');
+
+        return (
+          <div>
+            <h5 className="fs-14 fw-medium text-capitalize">
+              {newTitle || '-'}
+            </h5>
+          </div>
+        );
+      },
+    },
+    {
+      title: 'Requested By',
+      key: 'agent',
+      render: (item) => (
+        <span className="d-flex flex-column text-capitalize">
+          {item?.requested_by?.first_name && item?.requested_by?.last_name
+            ? `${
+                item?.requested_by?.first_name
+                  ? item?.requested_by?.first_name
+                  : ''
+              } ${
+                item?.requested_by?.last_name
+                  ? item?.requested_by?.last_name
+                  : ''
+              }`
+            : '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Requester Role',
+      key: 'role',
+      render: (item) => (
+        <span className="d-flex flex-column text-capitalize">
+          {item?.requested_by?.role ? item?.requested_by?.role : '-'}
+        </span>
+      ),
+    },
+
+    {
+      title: 'Requester Email',
+      key: 'email',
+      render: (item) => (
+        <div>
+          <h5 className="fs-14 fw-medium">
+            {`${item?.requested_by?.email ? item?.requested_by?.email : '-'}`}
+          </h5>
+        </div>
+      ),
+    },
+    {
+      title: 'Notes',
+      key: 'notes',
+      render: (item) => (
+        <div className="fs-14 fw-medium text-capitalize">
+          {`${item?.notes ? item?.notes : '-'}`}
+        </div>
+      ),
+    },
+    {
+      title: 'Submitted Files',
+      key: 'files',
+      render: (item) => (
+        <div>
+          {item?.files && item?.files.length > 0 ? (
+            <FileViewer files={item?.files && item?.files} />
+          ) : (
+            'No submission files yet'
+          )}
+        </div>
+      ),
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      render: (item) => (
+        <span
+          className={`d-flex flex-column text-capitalize fw-semibold ${
+            item?.status === 'accepted'
+              ? 'text-success'
+              : item?.status === 'rejected'
+                ? 'text-danger'
+                : item?.status === 'pending'
+                  ? 'text-warning'
+                  : item?.status === 'requested'
+                    ? 'text-primary'
+                    : item?.status === 'submitted'
+                      ? 'text-info'
+                      : ''
+          }`}
+        >
+          {item?.status ? <span>{item?.status}</span> : '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: (item) => {
+        if (item?.requested_by?.role === 'agent') {
+          return <span className="text-capitalize">-</span>;
+        }
+        return (
+          <button
+            onClick={() => togModal(item?._id)}
+            className="button d-flex px-3 py-1"
+          >
+            Upload
+          </button>
+        );
+      },
+    },
+  ];
+
   return (
     <Layout>
       <div className="page-content">
         <div className="h-100">
           <Card>
             <CardHeader>
-              <h3>
-                All Student Air Ticket Document Upload Requests from Agent
+              <h3 className="fs-1 fw-bold text-primary text-center py-3 ">
+                All Student Air Ticket Document Upload Requests from You
               </h3>
               <SearchComponent
-                searchTerm={searchTermForRequest}
-                handleSearchChange={handleSearchChangeForRequest}
+                searchTerm={searchTermForRequestForAgent}
+                handleSearchChange={handleSearchChangeForRequestForAgent}
               />
             </CardHeader>
             <CardBody>
@@ -469,12 +648,46 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
               ) : (
                 <CommonTableComponent
                   headers={docRequestTableHeaderDataWithoutAction}
-                  data={isfilteredData ? isfilteredData : []}
-                  currentPage={currentPageForRequest}
-                  setCurrentPage={setCurrentPageForRequest}
+                  data={isfilteredDataForAgent ? isfilteredDataForAgent : []}
+                  currentPage={currentPageForRequestForAgent}
+                  setCurrentPage={setCurrentPageForRequestForAgent}
                   perPageData={perPageDataForRequest}
-                  searchTerm={searchTermForRequest}
-                  handleSearchChange={handleSearchChangeForRequest}
+                  searchTerm={searchTermForRequestForAgent}
+                  handleSearchChange={handleSearchChangeForRequestForAgent}
+                  emptyMessage="No Data found yet."
+                />
+              )}
+            </CardBody>
+          </Card>
+          {/* For Super Admin Request doc Table */}
+          <Card>
+            <CardHeader>
+              <h3 className="fs-1 fw-bold text-primary text-center py-3 ">
+                All Student Air Ticket Document Upload Requests from Super Admin
+              </h3>
+              <SearchComponent
+                searchTerm={searchTermForRequestForSuperAdmin}
+                handleSearchChange={handleSearchChangeForRequestForSuperAdmin}
+              />
+            </CardHeader>
+            <CardBody>
+              {allDocumentRequestForAgentIsLoading ? (
+                <LoaderSpiner />
+              ) : allDocumentRequestForAgentError ? (
+                <div>Error loading data....</div>
+              ) : (
+                <CommonTableComponent
+                  headers={docRequestTableHeaderDataWithAction}
+                  data={
+                    isfilteredDataForSuperAdmin
+                      ? isfilteredDataForSuperAdmin
+                      : []
+                  }
+                  currentPage={currentPageForRequestForSuparAdmin}
+                  setCurrentPage={setCurrentPageForRequestForSuparAdmin}
+                  perPageData={perPageDataForRequest}
+                  searchTerm={searchTermForRequestForSuperAdmin}
+                  handleSearchChange={handleSearchChangeForRequestForSuperAdmin}
                   emptyMessage="No Data found yet."
                 />
               )}
@@ -482,7 +695,9 @@ const StudentAirtTicketDocumentUploadRquestForAgent = () => {
           </Card>
           <Card>
             <CardHeader>
-              <h3>All Student Air Ticket Document Submission Table</h3>
+              <h3 className="fs-1 fw-bold text-primary text-center py-3 ">
+                All Student Air Ticket Document Submission Table
+              </h3>
               <SearchComponent
                 searchTerm={searchTermForSubmitedData}
                 handleSearchChange={handleSearchChangeForSubmittedData}
