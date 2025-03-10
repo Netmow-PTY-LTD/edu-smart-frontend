baff8955f38cbd96ef85c1d7ca03db7bcdafb09e
rabby.dev <rabby.netmow@gmail.com>
2025-03-10 11:45:01 +0600

    refactor hader code

--- src/pages/dashboard/agent/familyTrip/index.jsx
+++ src/pages/dashboard/agent/familyTrip/index.jsx
@@ -1,10 +1,10 @@
 import CommonTableComponent from '@/components/common/CommonTableComponent';
-import ProgressBar from '@/components/common/ProgressBar';
+
 import LoaderSpiner from '@/components/constants/Loader/LoaderSpiner';
 import Layout from '@/components/layout';
 import { useGetAgentFamilyTripQuery } from '@/slice/services/agent/agentEarningsService';
+import DataObjectComponent from '@/utils/common/data';
 
-import moment from 'moment';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import {
@@ -15,7 +15,6 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
-  Progress,
   Row,
   UncontrolledDropdown,
 } from 'reactstrap';
@@ -26,105 +25,8 @@ export default function AgentFamilyTrip() {
   const router = useRouter();
   const { data: familyTrip, isLoading: familyTripLoading } =
     useGetAgentFamilyTripQuery();
-
-  const agentFamilyTripHeaders = [
-    {
-      title: 'SN',
-      key: 'sn',
-      render: (item, index) => (
-        <span className="d-flex flex-column text-capitalize">{index + 1}</span>
-      ),
-    },
-
-    // {
-    //   title: 'Package Name',
-    //   key: 'package_name',
-    //   render: (item) => (
-    //     <span className="d-flex flex-column text-capitalize">
-    //       {item?.package?.name || '-'}
-    //     </span>
-    //   ),
-    // },
-    // {
-    //   title: 'Package Price ($)',
-    //   key: 'package_price',
-    //   render: (item) => (
-    //     <span className="d-flex flex-column">
-    //       ${item?.package?.price || '-'}
-    //     </span>
-    //   ),
-    // },
-    // {
-    //   title: 'Package Duration',
-    //   key: 'package_duration',
-    //   render: (item) => (
-    //     <span className="d-flex flex-column text-capitalize">
-    //       {item?.package?.duration || '-'}
-    //     </span>
-    //   ),
-    // },
-    {
-      title: 'Target Start Date',
-      key: 'start_date',
-      render: (item) => (
-        <span className="d-flex flex-column">
-          {moment(item?.start_date).format('DD-MM-YYYY') || '-'}
-        </span>
-      ),
-    },
-    {
-      title: 'Target End Date',
-      key: 'end_date',
-      render: (item) => (
-        <span className="d-flex flex-column">
-          {moment(item?.end_date).format('DD-MM-YYYY') || '-'}
-        </span>
-      ),
-    },
-
-    {
-      title: 'Target Status',
-      key: 'target_status',
-      render: (item) => (
-        <ProgressBar
-          target={item?.target ?? 0}
-          targetAchieved={item?.target_achieved ?? 0}
-        />
-      ),
-    },
-
-    {
-      title: 'Status',
-      key: 'status',
-      render: (item) => (
-        <span
-          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
-            item?.status === 'active'
-              ? 'bg-success-subtle text-success'
-              : 'bg-danger-subtle text-danger'
-          }`}
-        >
-          {item?.status || '-'}
-        </span>
-      ),
-    },
-    {
-      title: 'Payout Status',
-      key: 'payout_status',
-      render: (item) => (
-        <span
-          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
-            item?.payout_status === 'paid'
-              ? 'bg-third-color text-primary'
-              : item?.payout_status === 'pending'
-                ? 'bg-warning-subtle text-warning'
-                : 'bg-danger-subtle text-danger'
-          }`}
-        >
-          {item?.payout_status || '-'}
-        </span>
-      ),
-    },
+  const { AGENT_FAMILYTRIP_HEADERS = [] } = DataObjectComponent();
+  const AGENT_FAMILYTRIP_HEADERS_ACTION = [
     {
       title: 'Action',
       key: 'actions',
@@ -174,7 +76,10 @@ export default function AgentFamilyTrip() {
                       </CardHeader>
                       <CardBody className="mh-100">
                         <CommonTableComponent
-                          headers={agentFamilyTripHeaders}
+                          headers={[
+                            ...AGENT_FAMILYTRIP_HEADERS,
+                            ...AGENT_FAMILYTRIP_HEADERS_ACTION,
+                          ]}
                           data={familyTrip?.data || []}
                           currentPage={currentPage}
                           setCurrentPage={setCurrentPage}
--- src/utils/common/data/index.js
+++ src/utils/common/data/index.js
@@ -4253,6 +4253,191 @@ const DataObjectComponent = () => {
     },
   ];
 
+  const AGENT_FAMILYTRIP_HEADERS = [
+    {
+      title: 'SN',
+      key: 'sn',
+      render: (item, index) => (
+        <span className="d-flex flex-column text-capitalize">{index + 1}</span>
+      ),
+    },
+
+    // {
+    //   title: 'Package Name',
+    //   key: 'package_name',
+    //   render: (item) => (
+    //     <span className="d-flex flex-column text-capitalize">
+    //       {item?.package?.name || '-'}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: 'Package Price ($)',
+    //   key: 'package_price',
+    //   render: (item) => (
+    //     <span className="d-flex flex-column">
+    //       ${item?.package?.price || '-'}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: 'Package Duration',
+    //   key: 'package_duration',
+    //   render: (item) => (
+    //     <span className="d-flex flex-column text-capitalize">
+    //       {item?.package?.duration || '-'}
+    //     </span>
+    //   ),
+    // },
+    {
+      title: 'Target Start Date',
+      key: 'start_date',
+      render: (item) => (
+        <span className="d-flex flex-column">
+          {moment(item?.start_date).format('DD-MM-YYYY') || '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Target End Date',
+      key: 'end_date',
+      render: (item) => (
+        <span className="d-flex flex-column">
+          {moment(item?.end_date).format('DD-MM-YYYY') || '-'}
+        </span>
+      ),
+    },
+
+    {
+      title: 'Target Status',
+      key: 'target_status',
+      render: (item) => (
+        <ProgressBar
+          target={item?.target ?? 0}
+          targetAchieved={item?.target_achieved ?? 0}
+        />
+      ),
+    },
+
+    {
+      title: 'Status',
+      key: 'status',
+      render: (item) => (
+        <span
+          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
+            item?.status === 'active'
+              ? 'bg-success-subtle text-success'
+              : 'bg-danger-subtle text-danger'
+          }`}
+        >
+          {item?.status || '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Payout Status',
+      key: 'payout_status',
+      render: (item) => (
+        <span
+          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
+            item?.payout_status === 'paid'
+              ? 'bg-third-color text-primary'
+              : item?.payout_status === 'pending'
+                ? 'bg-warning-subtle text-warning'
+                : 'bg-danger-subtle text-danger'
+          }`}
+        >
+          {item?.payout_status || '-'}
+        </span>
+      ),
+    },
+  ];
+  const AGENTYEARLYBONOUSHEADERS = [
+    {
+      title: 'SN',
+      key: 'sn',
+      render: (item, index) => (
+        <span className="d-flex flex-column text-capitalize">{index + 1}</span>
+      ),
+    },
+    // {
+    //   title: 'Package Name',
+    //   key: 'package_name',
+    //   render: (item) => (
+    //     <span className="d-flex flex-column text-capitalize">
+    //       {item?.package?.name || 'N/A'}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: 'Amount ($)',
+    //   key: 'amount',
+    //   render: (item) => (
+    //     <span className="d-flex flex-column">${item?.amount || '0'}</span>
+    //   ),
+    // },
+    {
+      title: 'Target Start Date',
+      key: 'start_date',
+      render: (item) => (
+        <span className="d-flex flex-column">
+          {moment(item?.start_date).format('DD-MM-YYYY') || '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Target End Date',
+      key: 'end_date',
+      render: (item) => (
+        <span className="d-flex flex-column">
+          {moment(item?.end_date).format('DD-MM-YYYY') || '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Target Status',
+      key: 'target_status',
+      render: (item) => (
+        <ProgressBar
+          target={item?.target ?? 0}
+          targetAchieved={item?.target_achieved ?? 0}
+        />
+      ),
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      render: (item) => (
+        <span
+          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
+            item?.status === 'active'
+              ? 'bg-success-subtle text-success'
+              : 'bg-danger-subtle text-danger'
+          }`}
+        >
+          {item?.status || '-'}
+        </span>
+      ),
+    },
+    {
+      title: 'Payout Status',
+      key: 'payout_status',
+      render: (item) => (
+        <span
+          className={`rounded-4 px-4 py-1 fw-medium text-capitalize ${
+            item?.payout_status === 'paid'
+              ? 'bg-third-color text-primary'
+              : item?.payout_status === 'pending'
+                ? 'bg-warning-subtle text-warning'
+                : 'bg-danger-subtle text-danger'
+          }`}
+        >
+          {item?.payout_status || '-'}
+        </span>
+      ),
+    },
+  ];
+
   return {
     TotalAgentPendingPayoutReportHeadersDataForSuperAdmin,
     TotalagentPayoutReportHeadersDataForSuperAdmin,
@@ -4312,6 +4497,8 @@ const DataObjectComponent = () => {
     docSubmittedTableHeaderDataWithoutAction,
     AGENT_YEARLYBONOUS_HEADERS_FOR_SUPER_ADMIN,
     AGENT_FAMILYTRIP_HEADERS_FOR_SUPER_ADMIN,
+    AGENT_FAMILYTRIP_HEADERS,
+    AGENTYEARLYBONOUSHEADERS,
   };
 };
 
