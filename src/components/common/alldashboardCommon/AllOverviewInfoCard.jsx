2c6fe9c8fadd7c945eb82a5591e9e29ba841574c
shadiknetmow <shadik.netmow@gmail.com>
2025-03-10 08:43:13 +0600

    Superadmin Student Profile to added agent Informations

--- src/components/common/alldashboardCommon/AllOverviewInfoCard.jsx
+++ src/components/common/alldashboardCommon/AllOverviewInfoCard.jsx
@@ -1,7 +1,11 @@
+import { useCustomData } from '@/utils/common/data/customeData';
+import Link from 'next/link';
 import React from 'react';
 import { Card, CardBody, CardTitle, Table } from 'reactstrap';
 
 const AllOverviewInfoCard = ({ data }) => {
+  const customData = useCustomData();
+
   return (
     <>
       <Card>
@@ -129,6 +133,84 @@ const AllOverviewInfoCard = ({ data }) => {
           </div>
         </CardBody>
       </Card>
+
+      {data?.agent && (
+        <Card>
+          <CardBody>
+            <CardTitle className="fw-semibold mb-3 text-primary">
+              Agent Information
+            </CardTitle>
+            <div className="table-responsive">
+              <Table className="table-borderless mb-0">
+                <tbody>
+                  <tr>
+                    <th className="ps-0" scope="row">
+                      Name
+                    </th>
+                    <td className="text-muted text-wrap d-flex align-items-center">
+                      <span className="me-2">:</span>
+                      <span className="text-capitalize">
+                        {data?.agent
+                          ? `${data.agent.first_name || ''} ${data.agent.last_name || ''}`.trim()
+                          : 'N/A'}
+                        {data?.agent?._id && (
+                          <Link
+                            href={`/dashboard/${customData?.paneltext}/agents/${data.agent._id}`}
+                            className="text-reset ms-2"
+                          >
+                            (View Profile)
+                          </Link>
+                        )}
+                      </span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="ps-0" scope="row">
+                      Phone
+                    </th>
+                    <td className="text-muted text-wrap d-flex">
+                      <span className="d-inline-block me-2">:</span>
+                      <span className="d-inline-block">{data.agent.phone}</span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="ps-0" scope="row">
+                      E-mail
+                    </th>
+                    <td className="text-muted text-wrap d-flex">
+                      <span className="d-inline-block me-2">:</span>
+                      <span className="d-inline-block">{data.agent.email}</span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="ps-0" scope="row">
+                      Location
+                    </th>
+                    <td className="text-muted text-wrap d-flex">
+                      <span className="d-inline-block me-2">:</span>
+                      <span className="d-inline-block">
+                        {[
+                          data.agent.address_line_1,
+                          data.agent.address_line_2,
+                          data.agent.city,
+                          data.agent.state,
+                          data.agent.zip,
+                          data.agent.country,
+                        ]
+                          .filter(Boolean) // Removes empty or undefined values
+                          .join(', ')}
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </CardBody>
+        </Card>
+      )}
     </>
   );
 };
