f7558a56eb20aa69c1283eab33520445cc069dfb
D M Shiblee Shadik <shadik.netmow@gmail.com>
2025-06-16 11:30:15 +0600

    Access

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -69,7 +69,9 @@ const DashBoardLayout = (props) => {
           '/dashboard/admission-manager/agents',
           '/dashboard/admission-manager/students',
           '/dashboard/admission-manager/settings/profile',
-          '/dashboard/admission-manager/university-management',
+          '/dashboard/admission-manager/university-management/all-university',
+          '/dashboard/admission-manager/university-management/single-university-profile/',
+          '/dashboard/admission-manager/university-management/edit-university/',
           '/dashboard/admission-manager/university-management/add-university',
         ];
 
