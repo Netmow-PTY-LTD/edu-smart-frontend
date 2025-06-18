de695763d4beb2a551ab6d6e471cf5ebcbe8bf51
D M Shiblee Shadik <shadik.netmow@gmail.com>
2025-06-18 16:19:34 +0600

    Access Menu

--- src/components/layout/sidebarLayoutData/SuperAdminSidebarData.jsx
+++ src/components/layout/sidebarLayoutData/SuperAdminSidebarData.jsx
@@ -59,141 +59,268 @@ const SuperAdminSidebarData = () => {
   
 
   const menuItems = [
-    {
-      id: 'dashboard',
-      label: 'Dashboard',
-      icon: 'ri-dashboard-2-line',
-      link: '/dashboard/' + paneltext,
+   {
+    id: 'dashboard',
+    label: 'Dashboard',
+    icon: 'ri-dashboard-2-line',
+    link: '/dashboard/' + paneltext,
+  },
+  {
+    id: 'universities',
+    label: 'Universities',
+    icon: 'ri-school-fill',
+    link: '/#',
+    click: (e) => {
+      e.preventDefault();
+      setIsUniversities(!isUniversities);
+      updateIconSidebar(e);
     },
-    {
-      id: 'universities',
-      label: 'Universities',
-      icon: 'ri-school-fill',
-      link: '/#',
-      click: (e) => {
-        e.preventDefault();
-        setIsUniversities(!isUniversities);
-        updateIconSidebar(e);
+    stateVariables: isUniversities,
+    subItems: [
+      {
+        id: 'alluniversity',
+        label: 'All University',
+        icon: 'ri-school-fill',
+        link: '/dashboard/' + paneltext + '/university-management/all-university',
+        parentId: 'universities',
       },
-      stateVariables: isUniversities,
-      subItems: [
-        {
-          id: 'alluniversity',
-          label: 'All University',
-          icon: 'ri-school-fill',
-          link: '/dashboard/' + paneltext + '/university-management/all-university',
-          parentId: 'universities',
-        },
-        {
-          id: 'adduniversity',
-          label: 'Add University',
-          icon: 'ri-school-fill',
-          link: '/dashboard/' + paneltext + '/university-management/add-university',
-          parentId: 'universities',
-        },
-      ],
-    },
-    {
-      id: 'students',
-      label: 'Students',
-      icon: 'ri-group-fill',
-      link: '/dashboard/' + paneltext + '/students',
-    },
-    {
-      id: 'package-management',
-      label: 'Package Management',
-      icon: 'ri-red-packet-fill',
-      link: '/#',
-      click: (e) => {
-        e.preventDefault();
-        setIsPackageManagement(!isPackageManagement);
-        updateIconSidebar(e);
+      {
+        id: 'adduniversity',
+        label: 'Add University',
+        icon: 'ri-school-fill',
+        link: '/dashboard/' + paneltext + '/university-management/add-university',
+        parentId: 'universities',
       },
-      stateVariables: isPackageManagement,
-      subItems: [
-        {
-          id: 'package',
-          label: 'Packages',
-          icon: 'ri-price-tag-2-fill',
-          link: '/dashboard/' + paneltext + '/packages',
-          parentId: 'package-management',
-        },
-        {
-          id: 'coupon',
-          label: 'Coupon Management',
-          icon: 'ri-coupon-3-fill',
-          link: '/dashboard/' + paneltext + '/coupon-management',
-          parentId: 'package-management',
-        },
-        {
-          id: 'hotoffer',
-          label: 'Hot Offers',
-          icon: 'ri-fire-fill',
-          link: '/dashboard/' + paneltext + '/hot-offer',
-          parentId: 'package-management',
-        },
-      ],
+    ],
+  },
+  {
+    id: 'allpermittedusers',
+    label: 'Roles & Permissions',
+    icon: 'ri-user-star-fill',
+    link: '/dashboard/' + paneltext + '/super-admin-panel/all-permitted-users',
+  },
+  {
+    id: 'recent-application',
+    label: 'Recent Application',
+    icon: 'ri-article-fill',
+    link: '/dashboard/' + paneltext + '/recent-application',
+  },
+  {
+    id: 'agents',
+    label: 'Partner',
+    icon: 'ri-group-2-fill',
+    link: '/dashboard/' + paneltext + '/agents',
+  },
+  {
+    id: 'students',
+    label: 'Students',
+    icon: 'ri-group-fill',
+    link: '/dashboard/' + paneltext + '/students',
+  },
+  {
+    id: 'alldocuments',
+    label: 'Document Required List',
+    icon: 'ri-file-copy-2-fill',
+    link: '/dashboard/' + `${paneltext}` + '/all-documents',
+  },
+  {
+    id: 'managedocument',
+    label: 'Manage Documents',
+    icon: 'ri-article-fill',
+    link: '/#',
+    click: function (e) {
+      e.preventDefault();
+      setIsManageDocument(!isManageDocument);
     },
-    {
-      id: 'super-admin-earnings',
-      label: 'Earnings',
-      icon: 'ri-coins-line',
-      link: '/#',
-      click: (e) => {
-        e.preventDefault();
-        setEarnings(!earnings);
+    stateVariables: isManageDocument,
+    subItems: [
+      {
+        id: 'alldocument',
+        label: 'Accepted Documents',
+        icon: 'ri-file-fill',
+        link: '/dashboard/' + `${paneltext}` + '/manage-documents/all-document-for-superAdmin',
+        pathName: '/dashboard/' + `${paneltext}` + '/manage-documents/all-document-for-superAdmin',
+        parentId: 'managedocument',
       },
-      stateVariables: earnings,
-      subItems: [
-        {
-          id: 'total-receive-amount',
-          label: 'Total Receive Amount',
-          icon: 'ri-money-rupee-circle-line',
-          link: '/dashboard/' + paneltext + '/super-admin-earnings/total-receive-amount',
-          parentId: 'super-admin-earnings',
-        },
-        {
-          id: 'super-admin-profit',
-          label: 'Super Admin Profit',
-          icon: 'ri-money-rupee-circle-line',
-          link: '/dashboard/' + paneltext + '/super-admin-earnings/super-admin-profit',
-          parentId: 'super-admin-earnings',
-        },
-      ],
-    },
-    {
-      id: 'alldocuments',
-      label: 'Document Required List',
-      icon: 'ri-file-copy-2-fill',
-      link: '/dashboard/' + paneltext + '/all-documents',
-    },
-    {
-      id: 'managedocument',
-      label: 'Manage Documents',
-      icon: 'ri-article-fill',
-      link: '/#',
-      click: (e) => {
-        e.preventDefault();
-        setIsManageDocument(!isManageDocument);
+      {
+        id: 'documentuploadrequest',
+        label: 'Requested & Submitted',
+        icon: 'ri-file-list-3-fill',
+        link: '/dashboard/' + `${paneltext}` + '/manage-documents/document-upload-request',
+        pathName: '/dashboard/' + `${paneltext}` + '/manage-documents/document-upload-request',
+        parentId: 'managedocument',
       },
-      stateVariables: isManageDocument,
-      subItems: [
-        {
-          id: 'alldocument',
-          label: 'Accepted Documents',
-          icon: 'ri-file-fill',
-          link: '/dashboard/' + paneltext + '/manage-documents/all-document-for-superAdmin',
-          parentId: 'managedocument',
-        },
-        {
-          id: 'documentuploadrequest',
-          label: 'Requested & Submitted',
-          icon: 'ri-file-list-3-fill',
-          link: '/dashboard/' + paneltext + '/manage-documents/document-upload-request',
-          parentId: 'managedocument',
-        },
-      ],
+      {
+        id: 'newRequest',
+        label: 'New Requests',
+        icon: 'ri-file-list-3-fill',
+        link: '/dashboard/' + `${paneltext}` + '/manage-documents/new-document-request',
+        pathName: '/dashboard/' + `${paneltext}` + '/manage-documents/new-document-request',
+        parentId: 'managedocument',
+      },
+    ],
+  },
+  {
+    id: 'airTicketDocumentuploadrequest',
+    label: 'Air Ticket Request',
+    icon: 'ri-ticket-2-line',
+    link: '/dashboard/' + `${paneltext}` + '/manage-air-ticket/air-ticket-upload-request',
+  },
+  {
+    id: 'airportpickupchargerequest',
+    label: 'Airport Pickup Request',
+    icon: 'ri-flight-takeoff-line',
+    link: '/dashboard/' + `${paneltext}` + '/airport-pickup-request',
+  },
+  {
+    id: 'report',
+    label: 'Report',
+    icon: 'ri-bank-card-line',
+    link: '/#',
+    click: function (e) {
+      e.preventDefault();
+      setIsPaymentReport(!isPaymentReport);
     },
+    stateVariables: isPaymentReport,
+    subItems: [
+      {
+        id: 'package-payment',
+        label: 'Package Payment',
+        icon: 'ri-red-packet-line',
+        link: '/dashboard/' + `${paneltext}` + '/payment-report/package-payment',
+        pathName: '/dashboard/' + `${paneltext}` + '/payment-report/package-payment',
+        parentId: 'report',
+      },
+      {
+        id: 'application-payment',
+        label: 'Application Payment',
+        icon: 'ri-box-1-line',
+        link: '/dashboard/' + `${paneltext}` + '/payment-report/application-payment',
+        pathName: '/dashboard/' + `${paneltext}` + '/payment-report/application-payment',
+        parentId: 'report',
+      },
+      {
+        id: 'familyTrip',
+        label: 'Family Trip',
+        icon: 'ri-gift-line',
+        link: '/dashboard/' + `${paneltext}` + '/familyTrip',
+        parentId: 'report',
+      },
+      {
+        id: 'yearlyBonous',
+        label: 'Yearly Bonus',
+        icon: 'ri-percent-line',
+        link: '/dashboard/' + `${paneltext}` + '/yearlyBonous',
+        parentId: 'report',
+      },
+    ],
+  },
+  {
+    id: 'blogs',
+    label: 'Blogs',
+    icon: 'ri-school-fill',
+    link: '/#',
+    click: function (e) {
+      e.preventDefault();
+      setIsBlogs(!isBlogs);
+      updateIconSidebar(e);
+    },
+    stateVariables: isBlogs,
+    subItems: [
+      {
+        id: 'addblog',
+        label: 'Add Blog',
+        icon: 'ri-school-fill',
+        link: '/dashboard/' + `${paneltext}` + '/blog/add-blog',
+        parentId: 'blogs',
+      },
+      {
+        id: 'allblogs',
+        label: 'Blogs List',
+        icon: 'ri-school-fill',
+        link: '/dashboard/' + `${paneltext}` + '/blog/blog-list',
+        parentId: 'blogs',
+      },
+    ],
+  },
+  {
+    id: 'contact-messages',
+    label: 'Contact Messages',
+    icon: 'ri-group-fill',
+    link: '/dashboard/' + `${paneltext}` + '/contact-messages',
+  },
+  {
+    id: 'subscription',
+    label: 'Subscription',
+    icon: 'ri-rss-line',
+    link: '/#',
+    click: function (e) {
+      e.preventDefault();
+      setSubscriptionManagement(!isSubscriptionManagement);
+    },
+    stateVariables: isSubscriptionManagement,
+    subItems: [
+      {
+        id: 'subscriptionList',
+        label: 'Subscription List',
+        icon: 'ri-rss-line',
+        link: '/dashboard/' + `${paneltext}` + '/subscription/subscription-list',
+        pathName: '/dashboard/' + `${paneltext}` + '/subscription/subscription-list',
+        parentId: 'subscription',
+      },
+    ],
+  },
+  {
+    id: 'management',
+    label: 'Team Management',
+    icon: 'ri-group-fill',
+    link: '/dashboard/' + `${paneltext}` + '/teams',
+  },
+  {
+    id: 'settings',
+    label: 'Settings',
+    icon: 'ri-settings-3-line',
+    link: '/#',
+    click: function (e) {
+      e.preventDefault();
+      setIsSettings(!isSettings);
+    },
+    stateVariables: isSettings,
+    subItems: [
+      {
+        id: 'paymentsettings',
+        label: 'Payment Settings',
+        icon: 'ri-refund-2-fill',
+        link: '/dashboard/' + `${paneltext}` + '/settings/payment',
+        pathName: '/dashboard/' + `${paneltext}` + '/settings/payment',
+        parentId: 'settings',
+      },
+      {
+        id: 'profilesettings',
+        label: 'Profile Settings',
+        icon: 'ri-user-settings-fill',
+        link: '/dashboard/' + `${paneltext}` + '/settings/profile',
+        pathName: '/dashboard/' + `${paneltext}` + '/settings/profile',
+        parentId: 'settings',
+      },
+      {
+        id: 'changeemail',
+        label: 'Change Email',
+        icon: 'ri-mail-add-fill',
+        link: '/dashboard/' + `${paneltext}` + '/settings/email',
+        pathName: '/dashboard/' + `${paneltext}` + '/settings/email',
+        parentId: 'settings',
+      },
+      {
+        id: 'changepassword',
+        label: 'Change Password',
+        icon: 'ri-lock-password-fill',
+        link: '/dashboard/' + `${paneltext}` + '/settings/password',
+        pathName: '/dashboard/' + `${paneltext}` + '/settings/password',
+        parentId: 'settings',
+      },
+    ],
+  },
 {
   id: 'access-management',
   label: 'Access Management',
